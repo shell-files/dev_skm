41297b2a009c2ba7009f5015d57203d2df5199ec
sooah-0208 <santacoyaki11@gmail.com>
2026-05-19 10:56:31 +0900

    settings 내용 추가(#36)

--- backend/src/utils/settings.py
+++ backend/src/utils/settings.py
@@ -4,13 +4,47 @@ class Settings(BaseSettings):
   host_ip: str
   domain: str
   # --------------------------
+  # kafka config
+  # --------------------------
+  kafka_server: str
+  kafka_topic: str
+  # --------------------------
+  # email config
+  # --------------------------
+  mail_username: str
+  mail_password: str
+  mail_from: str
+  mail_port: int = 587
+  mail_server: str = "smtp.gmail.com"
+  mail_from_name: str = "W.I.T.H"
+  mail_starttls: bool = True
+  mail_ssl_tls: bool = False
+  use_credentials: bool = True
+  validate_certs: bool = True
   # tokenset.py
   # --------------------------
+  # secret_key: str
   private_key: str = "secrets/authpr.pem"
   public_key: str = "secrets/authpb.pem"
   access_token_expire_minutes: int
   refresh_token_expire_days: int
   invite_token_expire_days: int
+  # --------------------------
+  # rediscl.py
+  # --------------------------
+  redis_host: str
+  redis_port: int
+  redis_db1: int
+  redis_db2: int
+  redis_db3: int
+  # --------------------------
+  # file.py
+  # --------------------------
+  service_key: str
+  # --------------------------
+  # ocr.py
+  # --------------------------
+  ocr_key_path: str = "secrets/ocr_key.json"
   # --------------------------
   # db.py
   # --------------------------
@@ -20,22 +54,7 @@ class Settings(BaseSettings):
   maria_db_database: str
   maria_db_port: int
   maria_db_key: str
-  # --------------------------
-  # auth.py
-  # --------------------------
   cookie_key: str
-  # --------------------------
-  # file.py
-  # --------------------------
-  file_dir: str
-  # --------------------------
-  # rediscl.py
-  # --------------------------
-  redis_host: str
-  redis_port: int
-  redis_db1: int
-  redis_db2: int
-  redis_db3: int
 
   model_config = SettingsConfigDict(
     env_file=".env",

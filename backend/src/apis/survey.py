7e7102f1a559e01b0a1c9044873467b960c9cead
채훈 <as2237as@gmail.com>
2026-06-03 17:30:17 +0900

    react에 맞게 API 리턴 (#118)

--- backend/src/apis/survey.py
+++ backend/src/apis/survey.py
@@ -17,7 +17,7 @@ async def createForm(req: CompanyModel, token=Depends(get_token)):
 
 @router.get("/export/{sheet_id}",
     summary="설문 결과 CSV 추출",
-    description="구글 시트 응답 데이터 CSV 생성"
+    description="구글 시트 응답 데이터 CSV 생성 DB 저장 X"
 )
 async def exportCsv(sheet_id: str, token=Depends(get_token)):
     return await exportCsvProcess(sheet_id, token)
